9cdc7f171af3abe00a5166213a2d60c3fba97f4c
Dhuvaraggn <dhuvaraggnajithraj@gmail.com>
2021-02-14 10:42:25 +0530

    make it better

---
+++ Iterator.py
@@ -0,0 +1,78 @@
+import numpy
+
+from scipy.sparse import coo_matrix
+
+from scipy import sparse
+from numpy import array
+
+class MatrixInfo:
+    row = numpy.array(list())
+    column = numpy.array(list())
+    value = numpy.array(list())
+    rowLength = 0
+    columnLength = 0
+
+    def __init__(self,row,column,value,rowLength,columnLength):
+        super().__init__()
+        self.row = row
+        self.column = column
+        self.value = value
+        self.rowLength = rowLength
+        self.columnLength = columnLength
+
+
+class Iterator:
+    error_limit = 0.01
+    iterationCount = 0
+    xConverged = sparse.coo_matrix([],dtype=float)
+    xMatrix = sparse.coo_matrix([],dtype=float)
+    pMatrix = sparse.coo_matrix([],dtype=float)
+
+    def __init__(self,xMatrix,pMatrix,error_limit = 0.001):
+        super().__init__()
+        self.xMatrix = xMatrix
+        self.pMatrix = pMatrix
+        self.error_limit = error_limit
+
+    def buildPageRank(self):
+        self.iterate(self.xMatrix.tocsr(),self.pMatrix.tocsr())
+
+    def iterate(self,x,p):
+        Iterator.iterationCount+=1
+#        print("Iteration count %d " % (Iterator.iterationCount))
+        y = x*p
+        diff = y-x
+        x = y
+        while not self.isConverged(diff):
+            Iterator.iterationCount+=1
+#            print("Iteration count %d " % (Iterator.iterationCount))
+            y = x*p
+            diff = y-x
+            x = y
+        self.xConverged = x.tocoo()
+
+
+
+
+    def isConverged(self,cx):
+        for value in zip(cx.data):
+            if abs(value[0]) > self.error_limit:
+                return False
+        return True
+
+    def __str__(self, *args, **kwargs):
+        desc = "Converged Matrix\n"
+        for i,j,v in zip(self.xConverged.row, self.xConverged.col, self.xConverged.data):
+             desc = desc + "Row: " + repr(i) + " Column: " + repr(j) + " Value: " + repr(format(v,".3f")) + "\n"
+        return desc
+
+
+
+
+
+
+
+
+
+
+
